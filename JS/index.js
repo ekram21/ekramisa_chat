01af9dcecc4ab232f28be6283f3a8aeced9d41c4
Ekram21 <ekram_lol@hotmail.com>
2018-09-07 03:46:10 +0600

    added auto update for chat for both

--- JS/index.js
+++ JS/index.js
@@ -11,13 +11,8 @@
 
   var database = firebase.database();
 
-  var Owner = 'Ekram';
-  var Friend = 'Ramisa';
-
-
-  FetchOwnMessagesTimeAndPopulate();
-  FetchFriendMessagesTimeAndPopulate();
-
+  $('.TextCont').fadeOut('fast');
+  
 function print(x){
     console.log(x);
 }
@@ -25,7 +20,7 @@ function print(x){
 
 
 
-function CreateSingleMessage(Recipient, Message_, OnlyTIME){
+function CreateSingleMessage(Recipient, messageWriter, Message_, OnlyTIME){
 
 
     var SingleMessage = document.createElement('div');
@@ -35,6 +30,12 @@ function CreateSingleMessage(Recipient, Message_, OnlyTIME){
 
         var MessageContOwner = document.createElement('div');
         MessageContOwner.setAttribute('class', 'MessageContOwner');
+
+        var MessageWriter = document.createElement('span');
+        MessageWriter.setAttribute('class', 'MessageWriter');
+
+        var t = document.createTextNode(messageWriter);
+        MessageWriter.append(t);
 
         var Time_ = document.createElement('span');
         Time_.setAttribute('class', 'Time_');
@@ -49,6 +50,7 @@ function CreateSingleMessage(Recipient, Message_, OnlyTIME){
         InnerMessage.append(t);
 
         MessageContOwner.append(Time_);
+        MessageContOwner.append(MessageWriter);
         MessageContOwner.append(InnerMessage);
 
         SingleMessage.append(MessageContOwner);
@@ -60,6 +62,12 @@ function CreateSingleMessage(Recipient, Message_, OnlyTIME){
 
         var MessageContFriend = document.createElement('div');
         MessageContFriend.setAttribute('class', 'MessageContFriend');
+
+        var MessageWriter = document.createElement('span');
+        MessageWriter.setAttribute('class', 'MessageWriter');
+
+        var t = document.createTextNode(messageWriter);
+        MessageWriter.append(t);
 
         var Time_ = document.createElement('span');
         Time_.setAttribute('class', 'Time_');
@@ -74,6 +82,7 @@ function CreateSingleMessage(Recipient, Message_, OnlyTIME){
         InnerMessage.append(t);
 
         MessageContFriend.append(Time_);
+        MessageContFriend.append(MessageWriter);
         MessageContFriend.append(InnerMessage);
 
         SingleMessage.append(MessageContFriend);
@@ -86,87 +95,137 @@ function CreateSingleMessage(Recipient, Message_, OnlyTIME){
 
 }
 
+document.getElementById('chooseFriendName_ID').addEventListener('click', e => {
+
+    $('.TextCont').fadeIn('slow');
+
+    $('.SingleMessage').remove();
+
+    friendName = document.getElementById('chooseFriendName_ID').getAttribute("data-main");
+
+    document.getElementById('FriendName_ID').value = friendName;
+
+    //FetchMessagesWithTimeAndPopulate(friendName);
+
+    var Owner = document.getElementById('LoginID').getAttribute("data-main");
+    var RoomID = 'MessageRoom' + Owner + friendName; //Ekram/MessageRoomEkramRamisa
+
+    address = Owner + '/' + RoomID + '/';
+
+    print(address);
+
+    AutoPollingUpdater(address);
+
+});
+
 
 
 document.getElementById('SendIcon').addEventListener('click', e => {
 
     var messageToBeSent = document.getElementById('MainInputText_Id').value;
 
+    var Owner = document.getElementById('LoginID').getAttribute("data-main");
+    var Friend = document.getElementById('FriendName_ID').value;
+    var messageWriter = Owner;
+
     var d = new Date();
     d.toUTCString();
 
     onlyTime = String(d).split(' ')[4];
 
-    print(String(onlyTime));
+   CreateSingleMessage('Owner', Owner, messageToBeSent, onlyTime);
 
-   CreateSingleMessage('Owner', messageToBeSent, onlyTime);
+   document.getElementById('MainInputText_Id').value = ''; //reset the input area to null
 
-   document.getElementById('MainInputText_Id').value = '';
 
-   var ref = database.ref(Owner + '/OwnMessages/');
 
-   var data = {
-        [onlyTime]: messageToBeSent
-   }
+   //DATABASE STUFF
+   //now enter this data in own firebase and friends firebase
+   var ownRoomID = 'MessageRoom' + Owner + Friend;
 
-   ref.update(data).then(function(){
-        $('.SingleMessage').remove();
-        FetchOwnMessagesTimeAndPopulate();
-        FetchFriendMessagesTimeAndPopulate();
+   var ref = database.ref(Owner + '/' + ownRoomID + '/'); // Ekram/MessageRoomEkramRamisa/
+
+   var data = {};
+
+   data[messageWriter] = {};
+
+   data[messageWriter] = {
+    [onlyTime] : messageToBeSent
+   };
+
+   ref.push(data).then(function(){
+        print('Successfully sent message!');
+
+        //now write this message in friends RoomID database
+
+        if (Friend!=null){
+            var friendRoomID = 'MessageRoom' + Friend + Owner;
+
+            var ref = database.ref(Friend + '/' + friendRoomID + '/');
+
+           var data = {};
+
+           data[messageWriter] = {};
+
+            data[messageWriter] = {
+                [onlyTime] : messageToBeSent
+            };
+
+            ref.push(data);
+        }
+
    })
 
 });
 
 
-function FetchOwnMessagesTimeAndPopulate(){
+function FetchMessagesWithTimeAndPopulate(FriendName){
+
+    var Owner = document.getElementById('LoginID').getAttribute("data-main");
+    var Friend = FriendName;
+    var messageWriter = Owner;
 
     TimeArray = [];
     MessageArray = [];
+    messageOwner = [];
 
-    var ref = database.ref(Owner + '/OwnMessages').once('value').then(function(snapshot){
+    var RoomID = 'MessageRoom' + Owner + Friend; //Ekram/MessageRoomEkramRamisa
+
+    var ref = database.ref(Owner + '/' + RoomID + '/').once('value').then(function(snapshot){
 
         var totalMessageJSON = snapshot.val();
 
-        var key;
+        var key; //this is chrono key
         for (key in totalMessageJSON){
-            TimeArray.push(key);
-            MessageArray.push(totalMessageJSON[key]);
-        }
 
-        print(TimeArray);
-        print(MessageArray);
+            ActualJSON = totalMessageJSON[key];
+
+            var key2; // this is messageWriter
+            for (key2 in ActualJSON){
+                messageOwner.push(key2);
+
+                var key3;
+                for (key3 in ActualJSON[key2]){
+                    TimeArray.push(key3);
+                    MessageArray.push(ActualJSON[key2][key3]);
+                }
+            }
+        }
 
         for (var i = 0; i < TimeArray.length; i++) {
 
-            CreateSingleMessage('Owner', MessageArray[i], TimeArray[i]);
+            recipient = '';
+            if (messageOwner[i] == Owner){
+                recipient = 'Owner';
+            }
+            else if (messageOwner[i] == Friend){
+                recipient = 'Friend';
+            }
+            else {
+                break;
+            }
 
-        }
-
-    });
-
-}
-
-function FetchFriendMessagesTimeAndPopulate(){
-
-    TimeArray_ = [];
-    MessageArray_ = [];
-
-    var ref = database.ref(Owner + '/FriendMessages').once('value').then(function(snapshot){
-
-        var totalMessageJSON = snapshot.val();
-
-        var key;
-        for (key in totalMessageJSON){
-            TimeArray_.push(key);
-            MessageArray_.push(totalMessageJSON[key]);
-        }
-
-        print(TimeArray_);
-        print(MessageArray_);
-
-        for (var i = 0; i < TimeArray_.length; i++) {
-
-            CreateSingleMessage('Friend', MessageArray_[i], TimeArray_[i]);
+            CreateSingleMessage(recipient, messageOwner[i], MessageArray[i], TimeArray[i]);
 
         }
 
@@ -175,4 +234,37 @@ function FetchFriendMessagesTimeAndPopulate(){
 }
 
 
- 
+
+
+
+
+
+function handleEvent(event, snapshot, optionalPreviousChildKey) {
+
+    console.log('Detected change in database, now repopulating..');
+
+    $('.SingleMessage').remove();
+
+    friendName = document.getElementById('chooseFriendName_ID').getAttribute("data-main");
+
+    FetchMessagesWithTimeAndPopulate(friendName);
+}
+
+function AutoPollingUpdater(address){
+    var ref = database.ref(address);
+
+    ref.limitToLast(1).on("child_added", function(snapshot, previousChildKey) { handleEvent("child_added", snapshot, previousChildKey); })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
